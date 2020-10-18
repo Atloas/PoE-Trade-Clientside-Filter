0e990e51d2ef9dbfa28e2a8adef73c568eb7692e
Atloas <miter010@gmail.com>
2020-10-18 22:02:36 +0200

    hides specified listings

--- Chrome/content.js
+++ Chrome/content.js
@@ -1,9 +1,12 @@
 chrome.runtime.onMessage.addListener (
     function(request, sender, sendResponse) {
         if(request.message === "PoEFilter-Activated") {
-            var characterNames = document.querySelectorAll(".character-name");
-            for (var i = 0; i < characterNames.length; i++) {
-                console.log(characterNames[i].innerHTML);
+            var characterNames = Array.from(document.querySelectorAll(".character-name"));
+            //TODO: Load this list from some Chrome extension storage medium
+            var filteredNames = ["Vurtelejka"];
+            var toHide = characterNames.filter(name => filteredNames.includes(name.innerHTML.substr(5)));
+            for (var i = 0; i < toHide.length; i++) {
+                toHide[i].closest(".row").style.display = "none";
             }
         }
     }
